78249ba4e314a4769f7a0a3964dfe32876a1e2b1
gloryarora <42442414+gloryarora@users.noreply.github.com>
2020-03-06 18:42:18 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,25 @@
 package com.epam4.task4;
-public class CalculateInterest {
-	//Calculating Simple Interest
-	public float calculateSimpleInterest(float Principle,float Time,float Rate)
-	{
-		float Simple_Interest;
-		Simple_Interest=(Principle*Time*Rate)/100;
-		return Simple_Interest;
-	}
-	//Calculating Compound Interest
-	public float calculateCompoundInterest(float Principle,float Time,float Rate)
-	{
-		float Compound_Interest;
-		Compound_Interest = (float)(Principle*(Math.pow((1+Rate/100),Time)));        
-		return Compound_Interest;
-	}
+
+import java.util.Scanner;
+
+public class Main {
+	private static Scanner sc;
+	private static float Principle,Time,Rate,Simple_Interest,Compound_Interest;
+
+	public static void main( String[] args )
+    {
+		sc = new Scanner(System.in);
+		System.out.println("Enter principle amount");
+		Principle = sc.nextFloat();
+		System.out.println("Enter time period");
+		Time = sc.nextFloat();
+		System.out.println("Enter rate of interest");
+		Rate = sc.nextFloat();
+		CalculateInterest ci =new CalculateInterest();
+		Simple_Interest=ci.calculateSimpleInterest(Principle,Time,Rate);
+		Compound_Interest=ci.calculateCompoundInterest(Principle,Time,Rate);
+		System.out.println("Your Simple Interest is "+Simple_Interest);
+		System.out.println("Your Compound Interest is "+Compound_Interest);
+        
+    }
 }
